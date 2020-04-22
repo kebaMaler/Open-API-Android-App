ef50019b846238e213d54074738ddaf88250fb98
Martin <sola.martin1@gmail.com>
2020-04-22 13:21:08 +0200

    Initial commit

---
+++ app/src/main/java/sola/martin/open_api_android_app/AuthActivity.kt
@@ -0,0 +1,12 @@
+package sola.martin.open_api_android_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
